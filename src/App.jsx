e199acce056e31f4f9a851d7de10e3c8b71ae10d
davicalebe <davicalebedon@gmail.com>
2026-02-03 21:04:42 -0300

    feat: add intro section structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import Intro from './components/intro'
 import Gallery from './components/gallery'
 import Testimonials from './components/testimonials'
 import About from './components/about'
+import background from './assets/background.jpg'
+import star from './assets/star.png'
+import calendar from './assets/calendar.png'
+import whatsapp from './assets/whatsapp.png'
+import instagram from './assets/instagram.png'
 import { useEffect, useRef } from "react"
 
 function App() {
@@ -24,21 +29,45 @@ function App() {
   }, [])
 
   return (
-    <main
-      ref={stageRef}
-      id="stage"
-      className="
-        h-screen w-screen
-        flex flex-row
-        overflow-x-scroll overflow-y-hidden
-        snap-x snap-mandatory
-      "
-    >
-      <Intro />
-      <Gallery />
-      <Testimonials />
-      <About />
-    </main>
+    <div className='relative min-h-screen overflow-hidden'>
+      <div className="flex justify-between px-16 fixed top-0 w-full h-10 bg-transparent">
+        <div className="flex gap-4">
+          <img src={star} alt="star" className="w-9 h-9" />
+          <img src={star} alt="star" className="w-9 h-9" />
+          <img src={star} alt="star" className="w-9 h-9" />
+          <img src={star} alt="star" className="w-9 h-9" />
+          <img src={star} alt="star" className="w-9 h-9" />
+        </div>
+
+        <div className="flex items-center justify-evenly bg-primary w-52 h-10 rounded-b-lg">
+          <img src={calendar} alt="calendar" className="w-8 h-8" />
+          <img src={whatsapp} alt="whatsapp" className="w-8 h-8" />
+          <img src={instagram} alt="instagram" className="w-8 h-8" />
+        </div>
+
+      </div>
+
+      <div
+        className="fixed inset-0 bg-cover bg-center -z-10"
+        style={{ backgroundImage: `url(${background})`}}
+      />    
+      <main
+        ref={stageRef}
+        id="stage"
+        className="
+          h-screen w-screen
+          flex flex-row
+          overflow-x-scroll overflow-y-hidden
+          scroll-smooth
+          snap-x snap-mandatory
+        "
+      >
+        <Intro />
+        <Gallery />
+        <Testimonials />
+        <About />
+      </main>
+    </div>
   )
 }
 
--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import profilepic from '../assets/IMG_0891.jpeg'
 
 const Intro = () => {
   return (
-    <section className='flex-shrink-0 w-screen h-screen snap-center snap-always'>
-        <h1>INTRO</h1>
+    <section className='relative flex items-center justify-center flex-shrink-0 w-screen h-screen snap-center snap-always overflow-hidden'>
+      <div className="absolute inset-0 -z-1 opacity-20 text-primary">
+        {Array.from({ length: 12 }).map((_, i) => (
+          <div
+            key={i}
+            className="text-[7vw] font-semibold tracking-widest break-all"
+          >
+            {Array.from({ length: 6 }).map((_, j) => (
+              <span key={j} className='font-audiowide'>ESPECIALISTA EM REALISMO </span>
+            ))}
+          </div>
+        ))}
+      </div>
+
+      <div className="relative">
+        <div className="w-[423px] h-[564px] rounded-full overflow-hidden mx-auto my-10 shadow-2xl shadow-red-100">
+          <img className='w-full h-full' src={profilepic} alt="will" />
+        </div>
+        <div className="flex items-center justify-center absolute bottom-1/8 left-1/2 -translate-x-1/2 z-40 bg-secondary w-xl h-20 rounded-3xl">
+          <span className="text-[2.5rem] font-audiowide text-primary tracking-widest">
+            @WILL_TATUADOR__
+          </span>
+        </div>
+        <div className="flex items-center justify-center absolute bottom-1 left-1/2 -translate-x-1/2 z-30 bg-primary w-lg h-20 rounded-3xl">
+          <span className="text-[2.5rem] font-audiowide text-secondary tracking-widest">
+            CONHEÇA MAIS
+          </span>
+        </div>
+      </div>
     </section>
   )
 }
