cf6fa88b6e67c05d45b6af40393122b72e2658b7
davicalebe <davicalebedon@gmail.com>
2026-02-25 19:34:20 -0300

    feat: add yt video model to testimonials page

--- src/pages/testimonials.jsx
+++ src/pages/testimonials.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Testimonials = () => {
   return (
-    <section className='flex-shrink-0 w-screen h-screen snap-center snap-always'>
-      <h1>DEPOIMENTOS</h1>
+    <section className='flex items-center justify-center flex-shrink-0 w-screen h-screen snap-center snap-always pb-10'>
+      <div className="flex items-center bg-black w-11/12 h-10/12 rounded-lg gap-2 p-2">
+        <iframe className='w-full h-full' src="https://www.youtube.com/embed/jLFs3GtAldk" title='testimonials' frameborder="0" allowFullScreen></iframe>
+{/*         <div className="flex flex-col">
+          <h1 className='text-2xl text-white'>Ver Transcrição</h1>
+          <p className='text-primary'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis quibusdam minus quos quam sapiente, quo quia eum, harum atque reprehenderit ducimus at, et blanditiis esse corporis qui similique! Fuga, consequuntur?
+          </p>
+        </div> */}
+      </div>
     </section>
   )
 }
