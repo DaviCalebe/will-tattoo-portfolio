0ac66246dbec660731e38f7e9c5bbe301fb637a9
davicalebe <davicalebedon@gmail.com>
2026-03-08 13:18:19 -0300

    feat: add glightbox to improve ux

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -1,4 +1,7 @@
+import { useEffect, useRef } from 'react'
 import Masonry from 'react-masonry-css'
+import GLightbox from 'glightbox'
+import "glightbox/dist/css/glightbox.min.css"
 import tattoo1 from "../assets/tattoos/t-arm-angel.webp"
 import tattoo2 from "../assets/tattoos/t-arm-eagle.webp"
 import tattoo3 from "../assets/tattoos/t-arm-jesus.webp"
@@ -20,7 +23,35 @@ import tattoo18 from "../assets/tattoos/t-arm-shelby.webp"
 import tattoo19 from "../assets/tattoos/t-arm-dragon.webp"
 import tattoo20 from "../assets/tattoos/t-arm-angel-2.webp"
 
+const tattoos = [
+  tattoo1, tattoo2, tattoo3,
+  tattoo4, tattoo5, tattoo6,
+  tattoo7, tattoo8, tattoo9,
+  tattoo10, tattoo11, tattoo12,
+  tattoo13, tattoo14, tattoo15,
+  tattoo16, tattoo17, tattoo18,
+  tattoo19, tattoo20,
+]
+
 const Gallery = () => {
+  const lightboxRef = useRef(null);
+
+  useEffect(() => {
+    lightboxRef.current?.destroy();
+
+    lightboxRef.current = GLightbox({
+      elements: tattoos.map((img, i) => ({
+        href: img,
+        type: "image",
+      })),
+    });
+
+    return () => {
+      lightboxRef.current?.destroy();
+      lightboxRef.current = null;
+    };
+  }, []);
+
   return (
     <section className='flex items-center justify-center flex-shrink-0 w-screen h-screen snap-center snap-always overflow-hidden pb-10'>
       <div className="grid w-11/12 h-10/12 overflow-hidden overflow-y-auto scrollbar scrollbar-thumb-primary scrollbar-thumb-rounded-full scrollbar-track-transparent rounded-lg p-2">
@@ -28,27 +59,18 @@ const Gallery = () => {
         className='my-masonry-grid'
         columnClassName='my-masonry-grid_column'
         >
-          <img className="rounded-lg" src={tattoo1} alt="Tattoo 1" />
-          <img className="rounded-lg" src={tattoo2} alt="Tattoo 2" />
-          <img className="rounded-lg" src={tattoo3} alt="Tattoo 3" />
-          <img className="rounded-lg" src={tattoo4} alt="Tattoo 4" />
-          <img className="rounded-lg" src={tattoo5} alt="Tattoo 5" />
-          <img className="rounded-lg" src={tattoo6} alt="Tattoo 6" />
-          <img className="rounded-lg" src={tattoo7} alt="Tattoo 7" />
-          <img className="rounded-lg" src={tattoo8} alt="Tattoo 8" />
-          <img className="rounded-lg" src={tattoo9} alt="Tattoo 9" />
-          <img className="rounded-lg" src={tattoo10} alt="Tattoo 10" />
-          <img className="rounded-lg" src={tattoo11} alt="Tattoo 11" />
-          <img className="rounded-lg" src={tattoo12} alt="Tattoo 12" />
-          <img className="rounded-lg" src={tattoo13} alt="Tattoo 13" />
-          <img className="rounded-lg" src={tattoo14} alt="Tattoo 14" />
-          <img className="rounded-lg" src={tattoo15} alt="Tattoo 15" />
-          <img className="rounded-lg" src={tattoo16} alt="Tattoo 16" />
-          <img className="rounded-lg" src={tattoo17} alt="Tattoo 17" />
-          <img className="rounded-lg" src={tattoo18} alt="Tattoo 18" />
-          <img className="rounded-lg" src={tattoo19} alt="Tattoo 19" />
-          <img className="rounded-lg" src={tattoo20} alt="Tattoo 20" />
-
+          {tattoos.map((img, i) => (
+            <a
+              key={i}
+              href={img}
+              onClick={(e) => {
+                e.preventDefault();
+                lightboxRef.current?.openAt(i);
+              }}
+            >
+              <img src={img} alt={`Tattoo ${i + 1}`} className='rounded-lg mb-4' />
+            </a>
+          ))}
         </Masonry>
       </div>
     </section>
