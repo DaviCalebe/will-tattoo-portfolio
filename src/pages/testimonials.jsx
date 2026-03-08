90db48ba174209e6fea9f4c2abd053caa5719161
davicalebe <davicalebedon@gmail.com>
2026-03-08 15:26:58 -0300

    feat: add testimonials page animation

--- src/pages/testimonials.jsx
+++ src/pages/testimonials.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { motion } from 'framer-motion'
 
 const Testimonials = () => {
   return (
     <section className='flex items-center justify-center flex-shrink-0 w-screen h-screen snap-center snap-always pb-10'>
-      <div className="flex items-center bg-black w-11/12 h-10/12 rounded-lg gap-2 p-2">
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        className="flex items-center bg-black w-11/12 h-10/12 rounded-lg gap-2 p-2">
         <iframe className='w-full h-full' src="https://www.youtube.com/embed/jLFs3GtAldk" title='testimonials' frameborder="0" allowFullScreen></iframe>
 {/*         <div className="flex flex-col">
           <h1 className='text-2xl text-white'>Ver Transcrição</h1>
@@ -11,7 +16,7 @@ const Testimonials = () => {
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis quibusdam minus quos quam sapiente, quo quia eum, harum atque reprehenderit ducimus at, et blanditiis esse corporis qui similique! Fuga, consequuntur?
           </p>
         </div> */}
-      </div>
+      </motion.div>
     </section>
   )
 }
